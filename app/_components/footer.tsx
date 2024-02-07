2358ad43cb14c45a18a5596b732b06fd858875c6
FernandoBlackPremium <92569976+FernandoBlackPremium@users.noreply.github.com>
2024-02-06 23:07:13 -0300

    style: ensure footer is alwayes on the bottom

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="w-full bg-primary py-6 px-5 bottom-0 left-0 right-0">
+    <div className="w-full bg-primary py-6 px-5">
       <p className="text-black text-xs font-bold opacity-75">© 2024 Copyright Luxor Barber</p>
     </div>
   );
